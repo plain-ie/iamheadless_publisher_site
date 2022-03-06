7f63f0d67a4439701a0156aa33b36d114db92a01
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-06 20:18:32 +0200

    fixed client

--- iamheadless_publisher_site/clients.py
+++ iamheadless_publisher_site/clients.py
@@ -39,7 +39,7 @@ class ModelClient(BaseClient):
             unpublished=None,
             ):
 
-        from iamheadless_publisher_api.lookups.items_retrieve import retrieve_items as publisher_retrieve_items
+        from iamheadless_publisher.lookups.items_retrieve import retrieve_items as publisher_retrieve_items
 
         data = publisher_retrieve_items(
             count=count,
@@ -48,14 +48,11 @@ class ModelClient(BaseClient):
             item_types=item_type,
             project_ids=project_id,
             page=page,
-            paginate=True,
             published=published,
             tenant_ids=tenant_id,
             unpublished=unpublished,
-            format='json'
+            format='dict'
         )
-
-        data = json.loads(data)
 
         data['results'] = settings.ITEM_TYPE_REGISTRY.serialize(data['results'], many=True)
 
@@ -68,23 +65,14 @@ class ModelClient(BaseClient):
             tenant_id=None,
             lookup_field='id',
             token=None,
-            format='json'
             ):
 
-        from iamheadless_publisher_api.lookups.item_retrieve import retrieve_item as publisher_retrieve_item
+        from iamheadless_publisher.lookups.item_retrieve import retrieve_item as publisher_retrieve_item
 
         data = publisher_retrieve_item(
             item_id,
             lookup_field=lookup_field,
-            format=format
+            format='dict'
         )
-
-        if data is None:
-            return None
-
-        if format == 'dict':
-            return data
-
-        data = json.loads(data)
 
         return settings.ITEM_TYPE_REGISTRY.serialize(data)
--- iamheadless_publisher_site/urls.py
+++ iamheadless_publisher_site/urls.py
@@ -1,7 +1,4 @@
 from .conf import settings
 
 
-print(settings.ITEM_TYPE_REGISTRY.item_types)
-
-
 urlpatterns = settings.ITEM_TYPE_REGISTRY.get_urlpatterns()
