7893877eeace9501424ec25a5b8b9df533d73047
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-09 16:28:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,35 @@ from distutils.core import setup
 
 
 setup(
+
+    # Application name:
     name="iamheadless_publisher_site",
-    description="#",
-    long_description=open("README.txt").read(),
-    url="#",
-    license="LICENSE",
+
+    # Version number (initial):
+    version="0.0.1",
+
+    # Application author details:
     author="Maris Erts",
     author_email="maris@plain.ie",
-    version="0.0.1",
-    include_package_data=True,
+
+    # Packages
     packages=["iamheadless_publisher_site"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="#",
+
+    #
+    license="LICENSE",
+    description="#",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
     install_requires=[
         "Django==4.0.1",
     ],
+
 )
